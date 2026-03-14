e7d54836873326941160dd49a4b14107006b76ba
m-chup <firemac908@gmail.com>
2026-03-13 17:22:22 -0700

    Update ArraysAssignment1.cpp

--- ArraysAssignment1.cpp
+++ ArraysAssignment1.cpp
@@ -1,38 +1,43 @@
-//add includes
-//add namespace declarations
+#include <iostream>
+#include <string>
+using namespace std;
+
 string lookupName(string targetName, string names[], string phoneNumbers[], int size);
 
 int main()
 {
-    using namespace std;
-    string names[] = {"Michael Myers",
-                      "Ash Williams",
-                      "Jack Torrance",
-                      "Freddy Krueger"};
-    string phoneNumbers[] = {"333-8000","333-2323",
-                             "333-6150","339-7970"};
+    string names[] = {"Michael Myers", "Ash Williams", "Jack Torrance", "Freddy Krueger"};
+    string phoneNumbers[] = {"333-8000","333-2323","333-6150","339-7970"};
     string targetName, targetPhone;
     char c;
-    do
-    {
-         cout << "Enter a name to find the "
-              << "corresponding phone number."
-              << endl;
+
+    do {
+         cout << "Enter a name to find the corresponding phone number." << endl;
          getline(cin, targetName);
-         targetPhone = lookupName(targetName,
-                           names, phoneNumbers,4);
+         targetPhone = lookupName(targetName, names, phoneNumbers,4);
          if (targetPhone.length() > 0)
-                 cout << "The number is: "
-                      << targetPhone << endl;
+         {
+            cout << "The number is: " << targetPhone << endl;
+            break;
+         }
          else
-                 cout << "Name not found. "
-                      << endl;
-         cout << "Look up another name? (y/n)"
-              << endl;
-      cin > c;
+         {
+            cout << "Name not found." << endl;
+            cout << "Look up another name? (y/n)" << endl;
+         }
+      cin >> c;
       cin.ignore();
- } while (c == 'y');
- return 0;
+    } while (c == 'y');
+    
+    return 0;
 }
 
-//complete lookupName unction
+string lookupName(string targetName, string names[], string phoneNumbers[], int size)
+{
+    for (int index = 0; index < size; index++)
+    {
+        if (names[index] == targetName)
+        return phoneNumbers[index];
+    }
+    return "";
+}
